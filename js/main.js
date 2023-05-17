5e08ec90dfb1a111e8f6115ccdfc3e73c2b9f5c1
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-17 15:35:15 +0200

    es + bonus

--- js/main.js
+++ js/main.js
@@ -4,14 +4,13 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-            emailPath: 'https://flynn.boolean.careers/exercises/api/random/mail',
             emailGenerate : [],
         }
     }, 
     methods: {},
     mounted() {
-            for (let index = 1; index < 10; index++) {
-                axios.get(this.emailPath).then((answer) => {
+            for (let index = 0; index < 10; index++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((answer) => {
                     this.emailGenerate.push(answer.data.response)
                 })
         }
