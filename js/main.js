6e91ce474d2b1af6912949e7bb3c933eeb780f9d
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-17 17:29:58 +0200

    add comment in html and js

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ createApp({
     }, 
     methods: {},
     mounted() {
+        //CREATE 10 EMAIL WITH A EmailPath(This is a api of Boolean) AND PUSH IN ARRAY 
             for (let index = 0; index < 10; index++) {
                 axios.get(this.emailPath).then((answer) => {
                     this.emailGenerate.push(answer.data.response)
