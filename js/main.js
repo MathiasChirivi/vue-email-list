bd744374d4740529dc57d8c8b772779a6150160e
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-17 15:15:34 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//console.log("Benvenuto User");
+
+const { createApp } = Vue
+createApp({
+    data(){
+        return{
+            emailPath: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            emailGenerate : [],
+        }
+    }, 
+    methods: {},
+    mounted() {
+            for (let index = 1; index < 10; index++) {
+                axios.get(this.emailPath).then((answer) => {
+                    this.emailGenerate.push(answer.data.response)
+                })
+        }
+    }
+}).mount('#app')
