222371ee1bf8c89d67fe79d1d65186fb0f03da18
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-17 15:56:57 +0200

    fix code main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,14 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
+            emailPath: "https://flynn.boolean.careers/exercises/api/random/mail",
             emailGenerate : [],
         }
     }, 
     methods: {},
     mounted() {
             for (let index = 0; index < 10; index++) {
-                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((answer) => {
+                axios.get(this.emailPath).then((answer) => {
                     this.emailGenerate.push(answer.data.response)
                 })
         }
